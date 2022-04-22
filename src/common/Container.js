e5c9060b67f44d1e89a1fc3130e257729225da10
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-04-22 16:38:09 +0200

    initial commit

---
+++ src/common/Container.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1260px;
+  padding: 20px;
+  margin: 0 auto;
+`;
---
+++ src/features/weatherPage/index.js
@@ -0,0 +1,19 @@
+import { Container } from "../../common/Container";
+
+
+export const WeatherPage = () => {
+
+  return (
+    <Container>
+      <section>
+        <form>
+          <input
+            type="text"
+
+          />
+
+        </form>
+      </section>
+    </Container>
+  )
+};
