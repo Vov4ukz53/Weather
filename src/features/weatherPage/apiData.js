8355b3c60dd567dd755002e436c538411eb599a9
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-04-23 12:25:43 +0200

    code formatted

---
+++ src/features/weatherPage/apiData.js
@@ -0,0 +1,2 @@
+export const API_KEY = "7758f89a16e34740af97b700ee003421";
+export const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
--- src/features/weatherPage/weatherPageSlice.js
+++ src/features/weatherPage/weatherPageSlice.js
@@ -16,17 +16,22 @@ const weatherSlice = createSlice({
       state.weather = weatherInfo;
       state.loading = false;
     },
+    fetchWeatherError: (state) => {
+      state.error = true;
+      state.loading = false;
+    },
   },
 });
 
 export const {
   fetchWeather,
-  fetchWeatherSuccess
+  fetchWeatherSuccess,
+  fetchWeatherError,
 } = weatherSlice.actions;
 
 const selectWeatherState = state => state.weather;
 
 export const selectWeather = state => selectWeatherState(state).weather;
-export const selectLoading = state => selectWeatherState(state).loading;
+export const selectError = state => selectWeatherState(state).error;
 
 export default weatherSlice.reducer;
