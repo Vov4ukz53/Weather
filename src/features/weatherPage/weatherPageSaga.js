dc392f1734e2f1ef921308ee0395dd255758096a
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-04-23 12:23:08 +0200

    code formatted

--- src/features/weatherPage/weatherPageSaga.js
+++ src/features/weatherPage/weatherPageSaga.js
@@ -1,10 +1,10 @@
 import { call, put, delay, takeLatest } from "redux-saga/effects";
-import { fetchWeather, fetchWeatherSuccess } from "./weatherPageSlice";
+import { fetchWeather, fetchWeatherError, fetchWeatherSuccess } from "./weatherPageSlice";
 import { getApi } from "./getApi";
-import { API_KEY } from "./apiUrl";
+import { API_KEY, API_URL } from "./apiData";
 
 function* fetchWeatherHandler({ payload: { query } }) {
-  const api = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`;
+  const api = `${API_URL}${query}&appid=${API_KEY}&units=metric`;
 
   try {
     yield delay(1000);
@@ -12,6 +12,7 @@ function* fetchWeatherHandler({ payload: { query } }) {
     yield put(fetchWeatherSuccess(weatherInfo));
   }
   catch (error) {
+    yield put(fetchWeatherError());
     console.error(error);
   }
 };
